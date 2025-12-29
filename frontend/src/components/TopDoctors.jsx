cd9eb3e66355cd4c684d1a2c32dceeb788450b7a
Pasindu Subasinghe <phimansa@gmail.com>
2025-12-30 02:50:44 +0530

    Banner component created

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { doctors } from "../assets/assets_frontend/assets";
+import { useNavigate } from "react-router-dom";
+//import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
+  const navigate = useNavigate();
+  //const { doctors } = useContext(AppContext);
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctor to Book</h1>
@@ -11,6 +15,7 @@ const TopDoctors = () => {
       <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 gap-y-6 pt-5 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
+            onClick={() => navigate(`/appointment/${item._id}`)}
             key={index}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer
                  transform transition-transform duration-500 hover:-translate-y-2"
@@ -34,7 +39,13 @@ const TopDoctors = () => {
         ))}
       </div>
 
-      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
         more
       </button>
     </div>
