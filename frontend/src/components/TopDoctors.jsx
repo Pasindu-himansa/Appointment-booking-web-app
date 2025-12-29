6237ea7f559e5a2e0753e0fffe4f8bd1b4a86f22
Pasindu Subasinghe <phimansa@gmail.com>
2025-12-30 00:19:29 +0530

    Top doctors component created

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -8,24 +8,32 @@ const TopDoctors = () => {
       <p className="sm:w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors.
       </p>
-      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+      <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 gap-y-6 pt-5 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
-            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
+            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer
+                 transform transition-transform duration-500 hover:-translate-y-2"
           >
-            <img className="bg-blue-50" src={item.image} alt="" />
-            <div p-4>
-              <div className="flex items-center gap-2 text-sm text-center text-green-500">
-                <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+            <img
+              className="bg-blue-50 w-full"
+              src={item.image}
+              alt={item.name}
+            />
+
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-green-500">
+                <span className="w-2 h-2 bg-green-500 rounded-full"></span>
                 <p>Available</p>
               </div>
-              <p>{item.name}</p>
-              <p>{item.speciality}</p>
+
+              <p className="mt-2 font-medium">{item.name}</p>
+              <p className="text-sm text-gray-600">{item.speciality}</p>
             </div>
           </div>
         ))}
       </div>
+
       <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
         more
       </button>
