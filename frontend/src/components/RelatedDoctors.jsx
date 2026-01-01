97fca9c98ac19a5f8443e89b3e52098e6fc01c62
Pasindu Subasinghe <phimansa@gmail.com>
2026-01-01 13:21:12 +0530

    related doctor component bug fixed

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
+
+const RelatedDoctors = ({ speciality, docId }) => {
+  const { doctors } = useContext(AppContext);
+  const navigate = useNavigate();
+  const [relDoc, setRelDoc] = useState([]);
+  useEffect(() => {
+    if (doctors.length > 0 && speciality) {
+      const doctorData = doctors.filter(
+        (doc) => doc.speciality === speciality && doc._id !== docId
+      );
+      setRelDoc(doctorData);
+    }
+  }, [doctors, speciality, docId]);
+
+  return (
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium">Top Doctor to Book</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors.
+      </p>
+      <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 gap-y-6 pt-5 px-3 sm:px-0">
+        {relDoc.slice(0, 5).map((item, index) => (
+          <div
+            onClick={() => navigate(`/appointment/${item._id}`)}
+            key={index}
+            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer
+                     transform transition-transform duration-500 hover:-translate-y-2"
+          >
+            <img
+              className="bg-blue-50 w-full"
+              src={item.image}
+              alt={item.name}
+            />
+
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-green-500">
+                <span className="w-2 h-2 bg-green-500 rounded-full"></span>
+                <p>Available</p>
+              </div>
+
+              <p className="mt-2 font-medium">{item.name}</p>
+              <p className="text-sm text-gray-600">{item.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          window.scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
+        more
+      </button>
+    </div>
+  );
+};
+
+export default RelatedDoctors;
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets_frontend/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -90,9 +91,9 @@ const Appointment = () => {
 
   return (
     docInfo && (
-      <div className="flex flex-col lg:flex-row gap-8">
+      <div className="flex-1 flex-col lg:flex-row gap-8">
         {/*------------------Doctor Details------------------ */}
-        <div>
+        <div className="flex-1 flex-col">
           <div>
             <img
               className="bg-blue-200  w-full sm:max-w-72 rounded-lg"
@@ -171,6 +172,9 @@ const Appointment = () => {
             Book an appointment
           </button>
         </div>
+        {/* Listing Related Doctors */}
+
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
       </div>
     )
   );
